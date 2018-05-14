7264b71a5f253262bbc7263960bff4db0e9b2968
Siddhartha Basu <biosidd@gmail.com>
2018-05-14 17:27:47 +0000

    Provides support for both tls and non-tls connection

--- connection.go
+++ connection.go
@@ -3,35 +3,45 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"time"
 
 	"github.com/arangodb/go-driver"
 	"github.com/arangodb/go-driver/http"
 )
 
-func getClient(host, port, user, pass string, isSync bool) (driver.Client, error) {
-	var c driver.Client
-	conn, err := http.NewConnection(
-		http.ConnectionConfig{
-			Endpoints: []string{
-				fmt.Sprintf("https://%s:%s", host, port),
-			},
-			TLSConfig: &tls.Config{InsecureSkipVerify: true},
+func getClient(host, port, user, pass string, isSecure bool) (driver.Client, error) {
+	var client driver.Client
+	var conn driver.Connection
+	if isSecure {
+		c, err := http.NewConnection(
+			http.ConnectionConfig{
+				Endpoints: []string{
+					fmt.Sprintf("https://%s:%s", host, port),
+				},
+				TLSConfig: &tls.Config{InsecureSkipVerify: true},
+			})
+		if err != nil {
+			return client, fmt.Errorf("could not connect %s", err)
+		}
+		conn = c
+	} else {
+		c, err := http.NewConnection(
+			http.ConnectionConfig{
+				Endpoints: []string{
+					fmt.Sprintf("http://%s:%s", host, port),
+				},
+			})
+		if err != nil {
+			return client, fmt.Errorf("could not connect %s", err)
+		}
+		conn = c
+	}
+	client, err := driver.NewClient(
+		driver.ClientConfig{
+			Connection:     conn,
+			Authentication: driver.BasicAuthentication(user, pass),
 		})
 	if err != nil {
-		return c, fmt.Errorf("could not connect %s", err)
-	}
-	config := driver.ClientConfig{
-		Connection:     conn,
-		Authentication: driver.BasicAuthentication(user, pass),
-	}
-	if isSync {
-		d, _ := time.ParseDuration("1s")
-		config.SynchronizeEndpointsInterval = d
-	}
-	client, err := driver.NewClient(config)
-	if err != nil {
-		return c, fmt.Errorf("could not get a client instance %s", err)
+		return client, fmt.Errorf("could not get a client instance %s", err)
 	}
 	return client, nil
 }
