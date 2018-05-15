10a3e5d5d95329762135922c7d9d7cba83016eea
Siddhartha Basu <biosidd@gmail.com>
2018-05-15 19:57:52 +0000

    Added two methods for managing database and collections

---
+++ database.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/arangodb/go-driver"
+	"github.com/sirupsen/logrus"
+)
+
+func runDatabaseAction(client driver.Client, db *Database, logger *logrus.Entry) error {
+	var adb driver.Database
+	switch db.Action {
+	case "create":
+		ok, err := client.DatabaseExists(context.Background(), db.Name)
+		if err != nil {
+			return fmt.Errorf("error in checking existence of database %s %s", db.Name, err)
+		}
+		if ok {
+			logger.Infof("database %s exists, nothing to create", db.Name)
+			return nil
+		}
+		adb, err = client.CreateDatabase(context.Background(), db.Name, nil)
+		if err != nil {
+			return fmt.Errorf("error in creating database %s %s", db.Name, err)
+		}
+		logger.Infof("created database %s", db.Name)
+	case "delete":
+		ok, err := client.DatabaseExists(context.Background(), db.Name)
+		if err != nil {
+			return fmt.Errorf("error in checking existence of database %s %s", db.Name, err)
+		}
+		if !ok {
+			logger.Infof("database %s does not exists, nothing to delete", db.Name)
+			return nil
+		}
+		adb, err := client.Database(context.Background(), db.Name)
+		if err != nil {
+			return fmt.Errorf("error in retrieving database %s %s", db.Name, err)
+		}
+		if err := adb.Remove(context.Background()); err != nil {
+			return fmt.Errorf("error in removing database %s %s", db.Name, err)
+		}
+		logger.Infof("removed database %s", db.Name)
+		return nil
+	default:
+		return fmt.Errorf("unsupported database action %s", db.Action)
+	}
+	for _, u := range db.Allowed {
+		ok, err := client.UserExists(context.Background(), u.User)
+		if err != nil {
+			return fmt.Errorf("error in checking for user %s %s", u.User, err)
+		}
+		if ok {
+			logger.Infof("user %s exists, skipping", u.User)
+			continue
+		}
+		dbUser, err := client.CreateUser(
+			context.Background(),
+			u.User,
+			&driver.UserOptions{
+				Password: u.Pass,
+			})
+		if err != nil {
+			return fmt.Errorf("error in creating user %s %s", u.User, err)
+		}
+		logger.Infof("successfully created user %s", u.User)
+		err = dbUser.SetDatabaseAccess(context.Background(), adb, getGrant(u.Grant))
+		if err != nil {
+			return fmt.Errorf(
+				"error in granting permission %s for user %s %s",
+				u.Grant,
+				u.User,
+				err,
+			)
+		}
+		logger.Infof("successfully granted permission %s to user %s", u.Grant, u.User)
+	}
+	return nil
+}
+
+func runCollectionAction(client driver.Client, coll *Collection, isEdge bool, logger *logrus.Entry) error {
+	ok, err := client.DatabaseExists(context.Background(), coll.Database)
+	if err != nil {
+		return fmt.Errorf("error in checking for database %s %s", coll.Database, err)
+	}
+	if !ok {
+		return fmt.Errorf("database %s does not exist, cannot create collection", coll.Database)
+	}
+	db, err := client.Database(context.Background(), coll.Database)
+	if err != nil {
+		return fmt.Errorf("error in retrieving database %s %s", coll.Database, err)
+	}
+	switch coll.Action {
+	case "create":
+		ok, err = db.CollectionExists(context.Background(), coll.Name)
+		if err != nil {
+			return fmt.Errorf("error in collection lookup %s", err)
+		}
+		if ok {
+			logger.Infof("collection %s exist, nothing to create", coll.Name)
+			return nil
+		}
+		opt := &driver.CreateCollectionOptions{}
+		if coll.Key.Increment != 0 {
+			opt.KeyOptions = &driver.CollectionKeyOptions{
+				Increment: coll.Key.Increment,
+				Offset:    coll.Key.Offset,
+			}
+		}
+		if isEdge {
+			opt.Type = driver.CollectionTypeEdge
+		}
+		_, err = db.CreateCollection(context.Background(), coll.Name, opt)
+		if err != nil {
+			fmt.Errorf("error in creating collection %s %s", coll.Name, err)
+		}
+		logger.Infof("successfully created collection %s", coll.Name)
+	default:
+		return fmt.Errorf("unsupported collection action %s", coll.Action)
+	}
+	return nil
+}
+
+func getGrant(g string) driver.Grant {
+	var grnt driver.Grant
+	switch g {
+	case "rw":
+		grnt = driver.GrantReadWrite
+	case "ro":
+		grnt = driver.GrantReadOnly
+	default:
+		grnt = driver.GrantNone
+	}
+	return grnt
+}
