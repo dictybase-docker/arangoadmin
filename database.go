6d4372c93b5a0993bda606aa3ab106da43569bfb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2026-03-13 14:00:23 -0500

    refactor(database): inline createDatabasePipeline into CreateDatabase and extend to full Pipe6

--- database.go
+++ database.go
@@ -4,60 +4,54 @@ import (
 	"context"
 	"fmt"
 
+	A "github.com/IBM/fp-go/v2/array"
 	E "github.com/IBM/fp-go/v2/either"
 	fperrors "github.com/IBM/fp-go/v2/errors"
 	F "github.com/IBM/fp-go/v2/function"
 	IO "github.com/IBM/fp-go/v2/io"
 	IOE "github.com/IBM/fp-go/v2/ioeither"
-	A "github.com/IBM/fp-go/v2/array"
 	driver "github.com/arangodb/go-driver"
 	"github.com/urfave/cli/v3"
 )
 
 // CreateDatabase creates one or more databases with optional user and grants
 func CreateDatabase(_ context.Context, cmd *cli.Command) error {
-	logger := newLogger(cmd)
-	connParams := connParamsFromCmd(cmd)
-	databases := cmd.StringSlice("database")
-	username := cmd.String("user")
-	password := cmd.String("password")
-	grant := cmd.String("grant")
-
-	result := F.Pipe1(
-		createArangoClient(connParams),
-		IOE.Chain(func(client driver.Client) IOE.IOEither[error, struct{}] {
-			return createDatabasePipeline(DatabaseParams{
-				WithClient: WithClient{Client: client, Logger: logger},
-				Databases:  databases,
-				Username:   username,
-				Password:   password,
-				Grant:      grant,
-			})
+	dbparams := DatabaseParams{
+		WithClient: WithClient{Logger: newLogger(cmd)},
+		Databases:  cmd.StringSlice("database"),
+		Username:   cmd.String("user"),
+		Password:   cmd.String("password"),
+		Grant:      cmd.String("grant"),
+	}
+	return F.Pipe6(
+		cmd,
+		connParamsFromCmd,
+		createArangoClient,
+		IOE.Map[error](func(client driver.Client) DatabaseParams {
+			dbparams.Client = client
+			return dbparams
 		}),
-	)
-
-	either := toEither(result)
-	return E.Fold(
-		F.Identity[error],
-		func(_ struct{}) error { return nil },
-	)(either)
-}
-
-// createDatabasePipeline creates all databases, then optionally creates user and grants access
-func createDatabasePipeline(p DatabaseParams) IOE.IOEither[error, struct{}] {
-	return F.Pipe3(
-		p.Databases,
-		A.Map(func(dbname string) DatabaseParams {
-			q := p
-			q.Dbname = dbname
-			return q
+		IOE.Chain(func(p DatabaseParams) IOE.IOEither[error, struct{}] {
+			return F.Pipe3(
+				p.Databases,
+				A.Map(func(dbname string) DatabaseParams {
+					q := p
+					q.Dbname = dbname
+					return q
+				}),
+				IOE.TraverseArraySeq(createSingleDatabase),
+				IOE.Chain(F.Ternary(
+					func(_ []struct{}) bool { return len(p.Username) > 0 },
+					F.Constant1[[]struct{}](createUserAndGrant(p)),
+					F.Constant1[[]struct{}](IOE.Of[error](struct{}{})),
+				)),
+			)
 		}),
-		IOE.TraverseArraySeq(createSingleDatabase),
-		IOE.Chain(F.Ternary(
-			func(_ []struct{}) bool { return len(p.Username) > 0 },
-			F.Constant1[[]struct{}](createUserAndGrant(p)),
-			F.Constant1[[]struct{}](IOE.Of[error](struct{}{})),
-		)),
+		toEither,
+		E.Fold(
+			F.Identity[error],
+			func(_ struct{}) error { return nil },
+		),
 	)
 }
 
@@ -67,12 +61,14 @@ func createSingleDatabase(p DatabaseParams) IOE.IOEither[error, struct{}] {
 		IOE.TryCatchError(func() (bool, error) {
 			return p.Client.DatabaseExists(context.Background(), p.Dbname)
 		}),
-		IOE.MapLeft[bool, error, error](fperrors.OnError(
+		IOE.MapLeft[bool](fperrors.OnError(
 			fmt.Sprintf("error checking for database %s", p.Dbname),
 		)),
 		IOE.Chain(F.Ternary(
 			F.Identity[bool],
-			F.Constant1[bool](IOE.FromIO[error](logDatabaseExists(p.Logger, p.Dbname))),
+			F.Constant1[bool](
+				IOE.FromIO[error](logDatabaseExists(p.Logger, p.Dbname)),
+			),
 			F.Constant1[bool](createDatabase(p)),
 		)),
 	)
@@ -82,10 +78,14 @@ func createSingleDatabase(p DatabaseParams) IOE.IOEither[error, struct{}] {
 func createDatabase(p DatabaseParams) IOE.IOEither[error, struct{}] {
 	return F.Pipe2(
 		IOE.TryCatchError(func() (struct{}, error) {
-			_, err := p.Client.CreateDatabase(context.Background(), p.Dbname, nil)
+			_, err := p.Client.CreateDatabase(
+				context.Background(),
+				p.Dbname,
+				nil,
+			)
 			return struct{}{}, err
 		}),
-		IOE.MapLeft[struct{}, error, error](fperrors.OnError(
+		IOE.MapLeft[struct{}](fperrors.OnError(
 			fmt.Sprintf("error creating database %s", p.Dbname),
 		)),
 		IOE.ChainFirstIOK[error](func(_ struct{}) IO.IO[struct{}] {
@@ -100,7 +100,7 @@ func createUserAndGrant(p DatabaseParams) IOE.IOEither[error, struct{}] {
 		IOE.TryCatchError(func() (bool, error) {
 			return p.Client.UserExists(context.Background(), p.Username)
 		}),
-		IOE.MapLeft[bool, error, error](fperrors.OnError(
+		IOE.MapLeft[bool](fperrors.OnError(
 			fmt.Sprintf("error checking for user %s", p.Username),
 		)),
 		IOE.Chain(F.Ternary(
@@ -110,7 +110,7 @@ func createUserAndGrant(p DatabaseParams) IOE.IOEither[error, struct{}] {
 					IOE.TryCatchError(func() (driver.User, error) {
 						return p.Client.User(context.Background(), p.Username)
 					}),
-					IOE.MapLeft[driver.User, error, error](fperrors.OnError(
+					IOE.MapLeft[driver.User](fperrors.OnError(
 						fmt.Sprintf("error fetching user %s", p.Username),
 					)),
 				)
@@ -124,12 +124,14 @@ func createUserAndGrant(p DatabaseParams) IOE.IOEither[error, struct{}] {
 							&driver.UserOptions{Password: p.Password},
 						)
 					}),
-					IOE.MapLeft[driver.User, error, error](fperrors.OnError(
+					IOE.MapLeft[driver.User](fperrors.OnError(
 						fmt.Sprintf("error creating user %s", p.Username),
 					)),
-					IOE.ChainFirstIOK[error](func(_ driver.User) IO.IO[struct{}] {
-						return logUserCreated(p.Logger, p.Username)
-					}),
+					IOE.ChainFirstIOK[error](
+						func(_ driver.User) IO.IO[struct{}] {
+							return logUserCreated(p.Logger, p.Username)
+						},
+					),
 				)
 			},
 		)),
@@ -142,7 +144,9 @@ func createUserAndGrant(p DatabaseParams) IOE.IOEither[error, struct{}] {
 					return UserWithGrant{Params: q, User: user}
 				}),
 				IOE.TraverseArraySeq(grantSingleDatabase),
-				IOE.Map[error](func(_ []struct{}) struct{} { return struct{}{} }),
+				IOE.Map[error](
+					func(_ []struct{}) struct{} { return struct{}{} },
+				),
 			)
 		}),
 	)
@@ -152,9 +156,12 @@ func createUserAndGrant(p DatabaseParams) IOE.IOEither[error, struct{}] {
 func grantSingleDatabase(u UserWithGrant) IOE.IOEither[error, struct{}] {
 	return F.Pipe2(
 		IOE.TryCatchError(func() (driver.Database, error) {
-			return u.Params.Client.Database(context.Background(), u.Params.Dbname)
+			return u.Params.Client.Database(
+				context.Background(),
+				u.Params.Dbname,
+			)
 		}),
-		IOE.MapLeft[driver.Database, error, error](fperrors.OnError(
+		IOE.MapLeft[driver.Database](fperrors.OnError(
 			fmt.Sprintf("error getting database %s", u.Params.Dbname),
 		)),
 		IOE.Chain(func(db driver.Database) IOE.IOEither[error, struct{}] {
@@ -166,8 +173,11 @@ func grantSingleDatabase(u UserWithGrant) IOE.IOEither[error, struct{}] {
 						getGrant(u.Params.Grant),
 					)
 				}),
-				IOE.MapLeft[struct{}, error, error](fperrors.OnError(
-					fmt.Sprintf("error granting access to database %s", u.Params.Dbname),
+				IOE.MapLeft[struct{}](fperrors.OnError(
+					fmt.Sprintf(
+						"error granting access to database %s",
+						u.Params.Dbname,
+					),
 				)),
 				IOE.ChainFirstIOK[error](func(_ struct{}) IO.IO[struct{}] {
 					return logGrantAccess(
